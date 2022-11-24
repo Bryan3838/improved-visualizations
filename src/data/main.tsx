a02340426aa8fe73974ba7665c4311dad14ee7c7
cheiss02 <Katherine.lazo01@student.csulb.edu>
2022-11-23 20:56:51 -0800

    Update main.tsx
    create the objects for the champions and roles

--- src/data/main.tsx
+++ src/data/main.tsx
@@ -1,40 +1,61 @@
 import { GLOBAL } from "./types/avg/GLOBAL";
 
-//File names
-const namesFiles = ['League of Legends Champion Stats 12.1','League of Legends Champion Stats 12.2','League of Legends Champion Stats 12.3','League of Legends Champion Stats 12.4','League of Legends Champion Stats 12.5','League of Legends Champion Stats 12.6','League of Legends Champion Stats 12.7','League of Legends Champion Stats 12.8','League of Legends Champion Stats 12.9','League of Legends Champion Stats 12.10','League of Legends Champion Stats 12.11','League of Legends Champion Stats 12.12','League of Legends Champion Stats 12.13','League of Legends Champion Stats 12.14','League of Legends Champion Stats 12.15','League of Legends Champion Stats 12.16','League of Legends Champion Stats 12.17','League of Legends Champion Stats 12.18','League of Legends Champion Stats 12.19','League of Legends Champion Stats 12.20']
+//Name of the files
+const namesFiles = [
+    'League of Legends Champion Stats 12.1',
+    'League of Legends Champion Stats 12.2',
+    'League of Legends Champion Stats 12.3',
+    'League of Legends Champion Stats 12.4',
+    'League of Legends Champion Stats 12.5',
+    'League of Legends Champion Stats 12.6',
+    'League of Legends Champion Stats 12.7',
+    'League of Legends Champion Stats 12.8',
+    'League of Legends Champion Stats 12.9',
+    'League of Legends Champion Stats 12.10',
+    'League of Legends Champion Stats 12.11',
+    'League of Legends Champion Stats 12.12',
+    'League of Legends Champion Stats 12.13',
+    'League of Legends Champion Stats 12.14',
+    'League of Legends Champion Stats 12.15',
+    'League of Legends Champion Stats 12.16',
+    'League of Legends Champion Stats 12.17',
+    'League of Legends Champion Stats 12.18',
+    'League of Legends Champion Stats 12.19',
+    'League of Legends Champion Stats 12.20'
+];
 
-//File extension
+//File extention
 const fileType = ".csv";
 
-//Folder where the files are located
+//Folder where the data is located
 const dataPath = "./archive";
 
-//Create an instance with object MAP with the data type that have been already define
+//Create an instance with the object MAP with the data types that were already defined
 export const DataFiles = new Map<string, Array<GLOBAL>>();
 
-//Charge the data
-export const loadData = async() =>{
+//Functions in charge of load the data in one
+export const loadData = async () => {
     for(const i of namesFiles){
-       
-        //Create an array to save the data per file
+
+        //Create an array to save the data for each archive
         const dataTypeMap = new Array<GLOBAL>();
 
-        //Create the name of the file as ./archive/League of Legends Champion Stats 12.1.csv this is for each file type
+        //Create the name of the archive as ./archive/League of Legends Champion Stats 12.1.csv that is for each file type
         const path = `${dataPath}/${i}${fileType}`;
-        console.log(`FOLDER ${path}`);
         
-        //Require the file to read it
+        //Require the archive to read it
         const file = require("".concat(path));
         await fetch(file)
             .then(response => response.text())
             .then(text => {
-                //Read each line
+                //Read the line
                 const dataRows = text.split("\n");
 
-                //Review the case of the file to read and build the data
+                //Review which is the case of the archive that is going to read and build the data
                 const dataArchive = new Array<GLOBAL>();
                 for(const dataRow of dataRows){
-                    //Return a new array when a space is found
+                    
+                    //return a new array when a space is found
                     const data = dataRow.split(";").filter(element => {
                         return element;
                     });
@@ -59,8 +80,43 @@ export const loadData = async() =>{
                 }
                 dataTypeMap.push(...dataArchive);
             })
-        
         DataFiles.set(i, dataTypeMap);
     }
-    console.log("DATA FILE", DataFiles);
+    return DataFiles
 }
+
+export const orderData = async () => {
+    const data = await loadData();
+    const keysObject = Array.from(data.keys())
+    const valuesObject = Array.from(data.values())
+    const newArr = new Array;
+    const dataFiltered = new Array;
+
+    for (let index = 0; index < valuesObject.length; index++) {
+        const element = valuesObject[index];
+        newArr.push({"name_file": keysObject[index], "value_file": element})
+    }
+
+    for (let index = 0; index < newArr.length; index++) {
+        const nameFile = newArr[index].name_file;
+        const element = newArr[index].value_file;
+
+        const champions:typeof element = {}
+        const roles:typeof element = {}
+        const item:typeof element = {}
+
+        element.reduce((prev: any, curr: any) => {
+            champions[curr.name] = element.filter((x: any) => x.name === curr.name)
+        });
+
+        element.reduce((prev: any, curr: any) => {
+            roles[curr.role] = element.filter((x: any) => x.role === curr.role)
+        });
+
+        item[nameFile] = {"champions": champions, "roles": roles}
+
+        dataFiltered.push(item);
+    }
+    console.log(dataFiltered);
+    return dataFiltered;
+};
